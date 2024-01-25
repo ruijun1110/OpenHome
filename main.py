348b8c7c46b684ccd848dfa23b3c69e04eb6a4be
Wang Ruijun <rwang724@usc.edu>
2024-01-24 17:53:33 -0800

    change main

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ load_dotenv()
 last_message = ""
 ACCESS_KEY = 'jsoYHt2GJ3T+6zAvfSgtzBRekNzQioEhisl97z2xQ/TuSRTJfNvphQ=='
 KEYWORD_FILE_PATH = '/Users/raymond/OpenHome/Hey-Open-Home_en_mac_v3_0_0.ppn'
-porcupine = pvporcupine.create(
-  access_key='jsoYHt2GJ3T+6zAvfSgtzBRekNzQioEhisl97z2xQ/TuSRTJfNvphQ==',
-  keyword_paths=['/Users/raymond/OpenHome/Hey-Open-Home_en_mac_v3_0_0.ppn']
-)
+# porcupine = pvporcupine.create(
+#   access_key='jsoYHt2GJ3T+6zAvfSgtzBRekNzQioEhisl97z2xQ/TuSRTJfNvphQ==',
+#   keyword_paths=['/Users/raymond/OpenHome/Hey-Open-Home_en_mac_v3_0_0.ppn']
+# )
 
 def Wake_Word_Detection():
    
